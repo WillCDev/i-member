bf1e0e89f4f6aee94a14c64f706ad8a085306307
Will CHadwick <will.chadwick@redington.co.uk>
2023-06-20 22:45:48 +0100

    lighten orange colour

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -8,7 +8,7 @@ export const Colors = {
   TiffanyBlue: '#94d2bd',
   Vanilla: '#e9d8a6',
   Gamboge: '#ee9b00',
-  AlloyOrange: '#ca6702',
+  AlloyOrange: '#da7712',
   Rust: '#bb3e03',
   Rufous: '#ae2012',
   Auburn: '#9b2226',
